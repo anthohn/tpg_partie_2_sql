1dabca691531c170ff88a2009cdd27c56e2dfce6
anthohn <anthonyhohn@gmail.com>
2025-05-06 10:44:29 +0200

    méthode lire données bdd

--- src/TestLignes.java
+++ src/TestLignes.java
@@ -1,3 +1,7 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TestLignes {
@@ -16,5 +20,51 @@ public class TestLignes {
         }
 
         System.out.println(hm.values());
+    }
+
+
+    public static ArrayList<Vehicule> lireDonnees() {
+        String url = "jdbc:sqlite:tpg.sqlite";
+        ArrayList<Vehicule> lstVehiculeDB = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String queryVehicules = "SELECT * FROM vehicules";
+            try (PreparedStatement stmtVehicules = conn.prepareStatement(queryVehicules)) {
+                try (ResultSet rsVehicules = stmtVehicules.executeQuery()) {
+                    while (rsVehicules.next()) {
+                        int prix = rsVehicules.getInt("prix_achat");
+                        int nbPassagers = rsVehicules.getInt("nb_passagers");
+
+                        String dateStr = rsVehicules.getString("date_acquisition");
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+                        LocalDate dateAchat = LocalDate.parse(dateStr,formatter);
+
+                        int km = rsVehicules.getInt("km");
+                        int tensionConso = rsVehicules.getInt("tension_conso");
+                        String moteurSecours = rsVehicules.getString("moteur_secours");
+                        String typeVehicule = rsVehicules.getString("type_vehicule");
+
+                        boolean moteurSecoursB = false ;
+                        if ("oui".equals(moteurSecours)) {
+                            moteurSecoursB = true;
+                        }
+
+                        Vehicule v ;
+                        if(typeVehicule.equals("trolley")){
+                            v = new Trolley(nbPassagers, prix, dateAchat, tensionConso, moteurSecoursB);
+                        }
+                        else {
+                            v = new Bus(nbPassagers, prix, dateAchat);
+                        }
+
+                        v.setKmAuCompteur(km);
+                        lstVehiculeDB.add(v);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lstVehiculeDB;
     }
 }
